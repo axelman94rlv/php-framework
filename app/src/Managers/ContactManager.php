8a522c9f4c879034a8ac4080de559548df5c571d
Axel Barbellion <axelbarbellion@gmail.com>
2024-11-30 19:07:46 +0100

    Exo 1 : Post méthode

---
+++ app/src/Managers/ContactManager.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Managers;
+
+use App\Entities\Contact;
+use App\Http\Request;
+use App\Http\Response;
+
+class ContactManager {
+    private string $filepath;
+
+    public function __construct(){
+        $this->filepath = __DIR__ .'/../../var/contacts';
+    }
+
+    
+
+    function validate(array $body, array $requiredFields): ?Response {
+        if (empty($body)) {
+            return new Response(
+                json_encode(['error' => 'Your contact is empty']),
+                400,
+                ['Content-Type' => 'application/json']
+            );
+        }
+        $missingFields = array_diff($requiredFields, array_keys($body));
+        if (!empty($missingFields)) {
+            return new Response(
+                json_encode(['error' => 'Missing fields: ' . implode(', ', $missingFields)]),
+                400,
+                ['Content-Type' => 'application/json']
+            );
+        }
+        if (count($body) !== count($requiredFields)) {
+            return new Response(
+                json_encode(['error' => 'Too much fields']),
+                400,
+                ['Content-Type' => 'application/json']
+            );
+        }
+        return null;
+    }
+    public function saveContact(Contact $contact): string {
+        $timestamp = time();
+        $filename = sprintf('%s_%s.json', $timestamp, $contact->getEmail());
+
+        $contact->setDateOfCreation($timestamp);
+        $contact->setDateOfLastUpdate($timestamp);
+
+        $filePath = $this->filepath . '/' . $filename;
+
+        file_put_contents($filePath, json_encode($contact->toArray(), JSON_PRETTY_PRINT));
+
+        return $filename;
+    }
+}
